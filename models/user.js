5a7c816bb7bf2cdef726cbae06571b0e716bdf6c
Deathnoval <dinhquanfananime@gmail.com>
2024-03-10 14:00:15 +0700

    commit4

--- models/user.js
+++ models/user.js
@@ -1,13 +1,44 @@
 const mongoose = require('mongoose');
 
-const userSchema =mongoose.Schema({
-    ho : String,
-    ten: String,
-    gender: String,
-    birthday:String,
-    email: String,
-    password: String,
+const userSchema =new mongoose.Schema({
+    ho : {
+        type: String,
+        required: true,
+    },
+    ten:  {
+        type: String,
+        required: true,
+    },
+    gender:  {
+        type: String,
+        required: true,
+    },
+    birthday: {
+        type: String,
+        required: true,
+    },
+    email:  {
+        type: String,
+        required: true,
+    },
+    password:  {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
     
-})
+});
+
+userSchema.virtual('id').get(function () {
+    return this.id.toHexString();
+});
+
+userSchema.set('toJSON', {
+    virtual: true,
+});
 
 exports.User=mongoose.model('User',userSchema,"Users")
+exports.userSchema=userSchema;
--- routers/users.js
+++ routers/users.js
@@ -1,6 +1,7 @@
 const {User}=require('../models/user');
 const express=require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 router.get(`/`, async (req,res)=> {
     const userList = await User.find();
@@ -11,5 +12,32 @@ router.get(`/`, async (req,res)=> {
     }
     res.send(userList);
 })
+
+router.get('/:id', async (req, res)=> {
+    const user=await User.findById(req.params.id);
+    
+    if(!user)
+    return res.status(500).json({message:'The user with the given ID was not found'})
+})
+
+router.post(`/`, async (req,res)=> {
+    let user = new User({
+        ho : req.body.ho,
+        ten:  req.body.ten,
+        gender:  req.body.gender,
+        birthday: req.body.birthday,
+        email:  req.body.email,
+        password:  bcrypt.hashSync(req.body.password,10),
+        isAdmin: req.body.isAdmin,
+        
+    })
+    //user = await user.save();
+
+    if(!user)
+    return res.status(400).send('the user cannot be found')
+
+    res.send({message: 'Đã gửi email xác nhận cho bạn'})
+    //res.send(user);
+});
 
 module.exports = router;
