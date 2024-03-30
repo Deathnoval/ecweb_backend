2c00da15badf5b47b22234f9ba93c9f1c58bc5dc
Deathnoval <dinhquanfananime@gmail.com>
2024-03-30 12:59:52 +0700

    commit 30/3 lan 6

--- routers/users.js
+++ routers/users.js
@@ -41,20 +41,23 @@ router.post("/", async (req, res) => {
 router.get("/:id/verify/:token/", async (req, res) => {
 	try {
 		const user = await User.findOne({ _id: req.params.id });
+        console.log(req.params.id);
 		if (!user) return res.status(400).send({ message: "Invalid link" });
 
 		const token = await Token.findOne({
 			userId: user._id,
 			token: req.params.token,
 		});
+        console.log(req.params.token);
 		if (!token) return res.status(400).send({ message: "Invalid link" });
 
-		await User.updateOne({ _id: user._id, verified: true });
-		await token.remove();
+		await User.findByIdAndUpdate({ _id: user._id}, {verified: true });
+		await token.deleteOne();
 
 		res.status(200).send({ message: "Email verified successfully" });
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error" });
+        console.log(error);
 	}
 });
 
