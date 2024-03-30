d163d4ae0ab31662ed64ee7f51f2d42f16e8e8d1
Deathnoval <dinhquanfananime@gmail.com>
2024-03-30 14:13:35 +0700

    commit 30/3 lan 11

--- routers/auth.js
+++ routers/auth.js
@@ -42,7 +42,8 @@ router.post("/", async (req, res) => {
 		}
 
 		const token = user.generateAuthToken();
-		res.status(200).send({ data: token, message: "logged in successfully" });
+		res.status(200).send({ data: token, message: "logged in successfully",success: true });
+        
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error" });
 	}
