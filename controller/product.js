d0878d8dd74afbdb3a785748a4cc864226bd1fa8
Deathnoval <dinhquanfananime@gmail.com>
2024-05-11 12:34:30 +0700

    11/5 lan 4

--- controller/product.js
+++ controller/product.js
@@ -4,31 +4,47 @@ const Product = require('../models/product');
 const getProductListALL = async (req, res) => {
     const type_get = req.params.type_get
     const value_sort = req.params.value_sort
+
     try {
-        if (type_get == "all") {
-            const productListAll = await Product.find();
-            const productListAll_DataFormat = productListAll.map(product => ({
-                id: product.product_id,
-                name: product.name,
-                price: product.price,
-                image: product.primary_image,
-                imageHover: product.image_hover,
-                color: product.array_color.map(arrayColor => ({
-                    name_color: arrayColor.name_color,
-                    code_color: arrayColor.code_color,
-                    image: arrayColor.image,
-                })),
-            }));
-            if (productListAll_DataFormat) {
-                res.json({ success: true, productListAll_DataFormat });
+        productListAll = await Product.find();
+        if (type_get != "all") {
+
+            productListAll = await Product.find({
+                category_id: type_get,
+
+
+            })
+            console.log(productListAll);
+            if (!(productListAll.length > 0)) {
+                console.log('Product');
+                productListAll = await Product.find({
+
+                    sub_category_id: type_get,
+                })
             }
-            else {
-                res.json({ success: false, message: "Lỗi truy xuất dữ liệu", color: "text-red-500" });
-            }
+            console.log(productListAll)
+
+        }
+        const productListAll_DataFormat = productListAll.map(product => ({
+            id: product.product_id,
+            name: product.name,
+            price: product.price,
+            image: product.primary_image,
+            imageHover: product.image_hover,
+            color: product.array_color.map(arrayColor => ({
+                name_color: arrayColor.name_color,
+                code_color: arrayColor.code_color,
+                image: arrayColor.image,
+            })),
+        }));
+        if (productListAll_DataFormat) {
+            res.json({ success: true, productListAll_DataFormat });
         }
         else {
-            res.json({ success: false, message: "Không có dữ liệu để truy xuất", color: "text-red-500" });
+            res.json({ success: false, message: "Lỗi truy xuất dữ liệu", color: "text-red-500" });
         }
+
+
 
     }
     catch (err) {
