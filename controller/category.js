6c65caf492914bb11ed825f8586a9d340521264f
Deathnoval <dinhquanfananime@gmail.com>
2024-05-24 13:10:21 +0700

    thêm api update_category

--- controller/category.js
+++ controller/category.js
@@ -125,30 +125,28 @@ const insertCategory = async (req, res) => {
 };
 
 
-const getAllCategoriesList=async(req,res)=>{
-  try{
+const getAllCategoriesList = async (req, res) => {
+  try {
     const categories = await Category.find().populate('sub_category');
-    if (categories)
-    {
+    if (categories) {
       const formattedData = categories.map(category => ({
         key: category.category_id, // Convert _id from ObjectId to string
         label: category.name, // Assuming 'icon' property exists
-        
-        }))
+
+      }))
       console.log(formattedData)
 
       res.json({ success: true, formattedData });
     }
   }
-  catch(err)
-  {
+  catch (err) {
     console.log(err);
     res.json({ success: false, message: "Lỗi truy xuất dữ liệu", color: "text-red-500" })
   }
 };
-const getSubCategory=async(req,res)=>{
+const getSubCategory = async (req, res) => {
   const { id } = req.params; // Lấy id từ URL
-  try{
+  try {
     const category = await Category.findOne({
       category_id: id,
     });
@@ -157,94 +155,113 @@ const getSubCategory=async(req,res)=>{
       const formattedSubCategories = category.sub_category.map(subCategory => ({
         key: subCategory.sub_category_id, // Convert _id from ObjectId to string
         label: subCategory.name, // Assuming 'name' property holds the label
-        
+
       }));
       console.log(formattedSubCategories)
       res.json({ success: true, formattedSubCategories });
     }
   }
-  catch(err)
-  {
+  catch (err) {
     console.log(err);
     res.json({ success: false, message: "Lỗi truy xuất dữ liệu", color: "text-red-500" })
   }
 };
 
-const add_primary_category= async(req,res)=>{
-  try{
-    let new_name_category=req.body.name;
-    console.log("new_name_category:"+new_name_category);
-    if(new_name_category.length==0)
-    {
-      return res.json({success: false, message: "Tên của danh mục không được để trống", color: "text-red-500"})
+const add_primary_category = async (req, res) => {
+  try {
+    let new_name_category = req.body.name;
+    console.log("new_name_category:" + new_name_category);
+    if (new_name_category.length == 0) {
+      return res.json({ success: false, message: "Tên của danh mục không được để trống", color: "text-red-500" })
     }
-    const existingCategory = await Category.findOne({ name:new_name_category });
-    if(existingCategory)
-    {
-      return res.json({success: false, message: "Tên của danh mục đã tồn tại", color: "text-red-500"})
-    }else
-    {
+    const existingCategory = await Category.findOne({ name: new_name_category });
+    if (existingCategory) {
+      return res.json({ success: false, message: "Tên của danh mục đã tồn tại", color: "text-red-500" })
+    } else {
       const new_route = `xem-tat-ca-${new_name_category.toLowerCase().replace(/ /g, '-')}`;
       let new_category_id;
       do {
         new_category_id = generateCategoryId();
         console.log(new_category_id);
-      } while (await Category.findOne({ category_id:new_category_id }));
+      } while (await Category.findOne({ category_id: new_category_id }));
 
-      const newCategory = Category({ name:new_name_category, category_id:new_category_id, route:new_route, sub_category: [] });
+      const newCategory = Category({ name: new_name_category, category_id: new_category_id, route: new_route, sub_category: [] });
       await newCategory.save();
-      res.json({success:true,message:"Thêm Danh mục chính thành công",color:"text-green-500"})
+      res.json({ success: true, message: "Thêm Danh mục chính thành công", color: "text-green-500" })
     }
-    
-  }catch(err)
-  {
+
+  } catch (err) {
     console.error(err);
     return res.json({ success: false, message: "Lỗi truy xuất dữ liệu", color: "text-red-500" })
 
   }
-  
+
 
 }
-const add_sub_category=async(req,res)=>{
-  try{
-    const {id}=req.params;
-    const new_name_sub_category=req.body.name_sub_category
-    console.log("new_name_sub_category:"+new_name_sub_category);
-    if(new_name_sub_category.length==0)
-    {
-      return res.json({success:false,message:"Tến danh mục phụ không được để trống",color:"text-red-500"});
+const add_sub_category = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const new_name_sub_category = req.body.name_sub_category
+    console.log("new_name_sub_category:" + new_name_sub_category);
+    if (new_name_sub_category.length == 0) {
+      return res.json({ success: false, message: "Tến danh mục phụ không được để trống", color: "text-red-500" });
     }
-    else{
-      const category = await Category.findOne({ category_id:id });
-      if(!category)
-      {
-        return res.json({success:false,message:"Category_id không tồn tại",color:"text-red-500"});
+    else {
+      const category = await Category.findOne({ category_id: id });
+      if (!category) {
+        return res.json({ success: false, message: "Category_id không tồn tại", color: "text-red-500" });
       }
-      else{
+      else {
         const new_route = `xem-tat-ca-${new_name_sub_category.toLowerCase().replace(/ /g, '-')}`;
         let new_sub_category_id;
         do {
           new_sub_category_id = generateCategoryId();
           console.log(new_sub_category_id);
         } while (category.sub_category.find(sub => sub.sub_category_id === new_sub_category_id));
-        category.sub_category.push({ sub_category_id:new_sub_category_id, name: new_name_sub_category, route:new_route });
+        category.sub_category.push({ sub_category_id: new_sub_category_id, name: new_name_sub_category, route: new_route });
         await category.save();
         res.json({
           success: true,
-          message:"Thêm danh mục phụ thành công",
-          color:"text-green-500"
+          message: "Thêm danh mục phụ thành công",
+          color: "text-green-500"
         })
+      }
     }
-  }
 
-  }catch(err){
+  } catch (err) {
     console.log(err);
-    res.json({success:false,message:"Lỗi truy xuất dữ liệu",colo:"text-red-500"});
+    res.json({ success: false, message: "Lỗi truy xuất dữ liệu", colo: "text-red-500" });
+  }
+}
+
+
+const update_Catergory = async (req, res) => {
+  const { new_name_category, category_id } = req.body
+  try {
+    const is_not_exists_category = await Category.find({ category_id: category_id })
+    if (!is_not_exists_category) {
+      return res.json({ success: false, message: "Không tìm thấy danh mục", colo: "text-red-500" });
+    }
+    else if (new_name_category.length == 0) {
+      return res.json({ success: false, message: "Tên danh mục không được để trống", color: "text-red-500" });
+    }
+    else {
+      const updateCategory = await Category.findOneAndUpdate({ category_id: category_id }, { name: new_name_category }, { new: true });
+      if (!updateCategory) {
+        console.log("lỗi update category");
+      }
+      else {
+        return res.json({ success: true, message: "Cập nhật tên danh mục thành công", color: "text-green-500" });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: "Lỗi truy xuất dữ liệu", colo: "text-red-500" });
   }
 }
 
 const deleteCategory = async (req, res) => { };
- 
+
 
 module.exports = {
   getCategories,
@@ -254,5 +271,6 @@ module.exports = {
   getAllCategoriesList,
   getSubCategory,
   add_primary_category,
-  add_sub_category
+  add_sub_category,
+  update_Catergory
 };
--- routers/category.js
+++ routers/category.js
@@ -7,10 +7,11 @@ router.get('/findCategory/:id', categoryController.getCategories);
 router.get('/getAllCategories', categoryController.getAllCategories);
 router.post('/insertCategory', categoryController.insertCategory);
 router.get('/deleteCategory', categoryController.deleteCategory);
-router.get('/getAllCategoriesList',categoryController.getAllCategoriesList);
-router.get('/getSubCategory/:id',categoryController.getSubCategory);
-router.post('/add_primary_category',categoryController.add_primary_category);
-router.post('/add_sub_category/:id',categoryController.add_sub_category);
+router.get('/getAllCategoriesList', categoryController.getAllCategoriesList);
+router.get('/getSubCategory/:id', categoryController.getSubCategory);
+router.post('/add_primary_category', categoryController.add_primary_category);
+router.post('/add_sub_category/:id', categoryController.add_sub_category);
+router.post('/update_Catergory', categoryController.update_Catergory);
 
 
 
