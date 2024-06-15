254a5d089f8ab14c9966ef8e6ef0d6582da84881
Deathhnoval <dinhquanfananime@gmail.com>
2024-06-15 23:12:43 +0700

    create cart

---
+++ controller/cart.js
@@ -0,0 +1,8 @@
+const Product=require("../models/product")
+const Category=require("../models/category")
+const add_to_cart=async(req,res)=>{
+    product_id=req.body.product_id
+    size=req.body.size
+    color=req.body.color
+    sl=req.body.sl
+}
