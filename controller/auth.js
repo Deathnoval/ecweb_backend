b300a806ffe0974f567e87aaf886008e9ceef4d1
Deathhnoval <dinhquanfananime@gmail.com>
2024-06-20 21:44:26 +0700

    check token 20/6

--- controller/auth.js
+++ controller/auth.js
@@ -66,7 +66,7 @@ const loginUser = async (req, res) => {
         res.cookie("accessTokens", accessTokens, {
           httpOnly: false,
           secure: true,
-          path: "/",
+          domain: "localhost",
           sameSite:"Lax",
           
 
--- controller/middleware.js
+++ controller/middleware.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = async (req, res, next) => {
     const token = req.header.token;
+    // const localStorageToken = localStorage.getItem('token');
+    console.log(localStorageToken)
     // console.log(token)
     if (token) {
         const accessToken = token.toString();
@@ -13,7 +15,7 @@ const verifyToken = async (req, res, next) => {
             }
             req.user = user;
             // console.log(user);
-
+            
             next();
         });
     } else {
